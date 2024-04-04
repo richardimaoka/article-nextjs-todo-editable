818934ff08587209882ad31be6750578a1db07ee
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-04-04 22:37:19 +0900

    html

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,6 +1,13 @@
-
 export default function Home() {
   return (
-    <main>empty</main>
+    <main>
+      <div>Todo with Server Actions</div>
+      <input />
+      <div>
+        <input type="checkbox" />
+        <div>Settle with ...</div>
+        <button>x</button>
+      </div>
+    </main>
   );
 }
